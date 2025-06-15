871a048cca927352c5a83bf2643262aaba9c2d9d
Harold Putman <h.putman@ieee.org>
2025-06-14 21:53:16 -0400

    Starting to build database

---
+++ migrations/1749951833566_create-students.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('students', {
+        id: 'id',
+        firstName: { type: 'varchar(100)', notNull: true },
+        lastName: { type: 'varchar(100)', notNull: true },
+        dob: { type: 'date', notNull: true },
+        createdAt: {
+          type: 'timestamp',
+          notNull: true,
+          default: pgm.func('current_timestamp'),
+        },
+      });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {};
