590d2fd195a5b8900123f57bd111d31398a7addc
Harold Putman <h.putman@ieee.org>
2025-12-20 15:19:02 -0500

    configure sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,19 @@
 import { defineConfig } from 'astro/config';
 import { remarkModifiedTime } from './remark-modified-time.mjs';
 import icon from 'astro-icon';
-
 import mdx from '@astrojs/mdx';
-
+import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ensemblesinc.org',
+
   markdown: {
     remarkPlugins: [remarkModifiedTime],
   },
 
-  integrations: [mdx(), icon()],
+  integrations: [mdx(), icon(), sitemap()],
 
   vite: {
     envPrefix: ['PUBLIC_'],
